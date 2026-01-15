89fa5854d4de2cdd9193d18052f2907da0a954b0
amele2323 <amelia_rkn@teknokrat.ac.id>
2026-01-15 11:49:17 +0700

    add CORS middleware

--- backend/internal/middleware/cors.go
+++ backend/internal/middleware/cors.go
@@ -1,7 +1,7 @@
 package middleware
 
 import "net/http"
-
+//
 func CORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
